601c5f03d27b373182565014c34fd298ab0c1274
Joe Hainline <joseph.hainline@gmail.com>
2013-01-08 00:23:42 -0600

    Prep for building a table view controller to view the numbers in.

--- JHPrimeNumberViewer/JHAppDelegate.m
+++ JHPrimeNumberViewer/JHAppDelegate.m
@@ -10,8 +10,7 @@
 
 @implementation JHAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
